c33d07a330d86613c797fd03d030d92105bea971
Muhammad-Arfan786 <mianarfanaslamgg@gmail.com>
2025-03-06 00:06:58 +0500

    Update grocery item.cs
    grocery item.cs

--- grocery item.cs
+++ grocery item.cs
@@ -6,6 +6,11 @@ class Program
     static void Main()
     {
         List<string> groceries = new List<string>()
+        git checkout 1st_branch
+nano grocery_list.cs  # Add an item on line 9
+git add grocery_list.cs
+git commit -m "Updated line 9 in 1st_branch"
+git push origin 1st_branch
         {
             "Milk",
             "Eggs",
